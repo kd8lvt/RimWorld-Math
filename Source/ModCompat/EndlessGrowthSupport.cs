9ce5d49066616a18f026e4df55e232c945628835
kittycat2002 <josh.masters.02@gmail.com>
2023-11-10 22:00:02 -0500

    Added support for the Endless Growth Mod

---
+++ Source/ModCompat/EndlessGrowthSupport.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace CrunchyDuck.Math.ModCompat {
+	public class EndlessGrowthSupport {
+
+        private static Type EGCommonPatchesType { get; } = Type.GetType("SlimeSenpai.EndlessGrowth.CommonPatches, EndlessGrowth");
+
+        private static readonly Func<int> EGGetMaxLevelForBill = AccessTools.MethodDelegate<Func<int>>(AccessTools.Method(EGCommonPatchesType, "GetMaxLevelForBill", Type.EmptyTypes));
+
+        public static int GetMaxLevelForBill()
+        {
+            return EGGetMaxLevelForBill();
+        }
+    }
+}
